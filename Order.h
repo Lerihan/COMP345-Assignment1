089b9c2c4ec58be6592194005481d7014c0298ef
StellarMemory <nuojie.cheng@mail.concordia.ca>
2020-10-03 15:20:24 -0400

    order.h

--- Order.h
+++ Order.h
@@ -5,6 +5,8 @@
 
 class Player;
 
+class Territory;
+
 class Order
 {
 public:
@@ -15,14 +17,98 @@ public:
 
 	virtual void execute() = 0;
 
+private:
+	Player* player;
 };
 
 class Deploy : public Order
 {
-	Deploy(Player* player, std::string armyName);
+public:
+
+	Deploy(Player* player, Territory* territory, unsigned int numOfArmies);
 	
-	virtual bool validate();
+	virtual bool validate() override;
 
-	virtual void execute();
+	virtual void execute() override;
 
+private:
+	Territory* territory;
+	unsigned int numOfArmies;
 };
+
+class Advance : public Order
+{
+public:
+
+	Advance(Player* player, Territory* current, Territory* adjacent, unsigned int numOfArmies);
+
+	virtual bool validate() override;
+
+	virtual void execute() override;
+
+private:
+	Territory* current;
+	Territory* adjacent;
+	unsigned int numOfArmies;
+};
+
+class Bomb : public Order
+{
+public:
+
+	Bomb(Player* player, Territory* target);
+
+	virtual bool validate() override;
+
+	virtual void execute() override;
+
+private:
+	Territory* target;
+};
+
+class Blockade : public Order
+{
+public:
+
+	Blockade(Player* player, Territory* target);
+
+	virtual bool validate() override;
+
+	virtual void execute() override;
+
+private:
+	Territory* target;
+};
+
+class Airlift : public Order
+{
+public:
+
+	Airlift(Player* player, Territory* current, Territory* next, unsigned int numOfArmies);
+
+	virtual bool validate() override;
+
+	virtual void execute() override;
+
+private:
+	Territory* current;
+	Territory* next;
+	unsigned int numOfArmies;
+};
+
+class Negotiate : public Order
+{
+public:
+
+	Negotiate(Player* current, Player* enemy);
+
+	virtual bool validate() override;
+
+	virtual void execute() override;
+
+private:
+	Player* enemy;
+};
+
+std::ostream & operator << (std::ostream &out, const Order &o);
+
