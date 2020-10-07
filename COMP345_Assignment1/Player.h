f187ad7c0df95472e3b0ccffc8fdcf654df8c836
Lerihan <phan.melina@gmail.com>
2020-10-07 18:39:45 -0400

    order/player uncommented (order gives errors)

--- COMP345_Assignment1/Player.h
+++ COMP345_Assignment1/Player.h
@@ -31,7 +31,7 @@ Copy constructor and = assignment operator assumes all other class assignment op
 private:
 	//static int playerCount; // to keep count of number of players, also needed to initialize playerNumber member
 	int playerNumber; // assign each player a number to keep track of which player is playing
-	std::vector<Order*> orders; // player's list of orders to be carried out
+	//std::vector<Order*> orders; // player's list of orders to be carried out
 	std::vector<Cards*> hand; // player's hand of cards
 	std::vector<Territory*> territories; // territories belonging to the player
 
@@ -40,7 +40,7 @@ public:
 	Player(Player& p); // copy constructor
 	~Player(); // destructor
 	vector<Territory*> getTerritories();
-	Order* getNextOrder(); // returns next order to be executed (first order in attribute vector)
+	//Order* getNextOrder(); // returns next order to be executed (first order in attribute vector)
 	void issueOrder(string orderType);
 	vector<Territory*> toDefend();
 	vector<Territory*> toAttack();
