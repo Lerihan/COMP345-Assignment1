82855ff85b14753be0ef4ab58b0231c9bbd3f4c3
Dodo <duchuybui5@hotmail.com>
2020-10-08 20:15:46 -0400

    final version of Cards!

--- COMP345_Assignment1/Cards.h
+++ COMP345_Assignment1/Cards.h
@@ -7,43 +7,56 @@
 
 #include <iostream>
 #include <string>
+#include "Player.h"
+#include "Order.h"
+
 using namespace std;
 
-class Deck;
 
 class Player;
 
-class Cards {
+class Deck;
+
+class Cards {	
 protected:
-	char *cardsAvailable[5];
+	static char *cardsAvailable[5];
 public:
 	Cards();
 	Cards(Cards& c);
 	Cards(char* newCards[5]);
-	//must be able to change array size somehow
+	//we are using 5 different types of cards
+	Cards& operator = (const Cards &c);
+	friend ostream& operator << (ostream& out, const Cards& c);
+	friend istream& operator << (istream& in, const Cards& c);
 };
 
-class Hand : public Cards {
-	friend class Deck;
+class Hand : public Cards{
+friend class Deck;
 protected:
 	Player* player;
-	string cardsInHand[5];
-	//must be able to change array size somehowpublic:
+	char*cardsInHand[5];
+	//a standard game of risk, which is what warzone is based of off, has 5 cards in the hand
 public:
 	Hand();
 	Hand(Hand& player);
 	Hand(Player* player, char* cardsInHand[5]);
-	void play(Hand& h, Deck& d);
+	Hand& operator = (const Hand &h);
+	friend ostream& operator << (ostream& out, const Hand& h);
+	friend istream& operator << (istream& in, const Hand& h);
+	static void play(char* cardToPlay, Deck& d, Hand& h);
 };
 
-class Deck : public Cards {
-	friend class Hand;
+class Deck : public Cards{
+friend class Hand;
 protected:
-	char* cardsInDeck[52];
-	//must be able to change array size somehow
+	char* cardsInDeck[55];
+	//a standard game of risk, which is what warzone is based of off, has 55 cards in the deck
 public:
 	Deck();
 	Deck(Deck& d);
-	Deck(char* cardsInDeck[52]);
-	void draw(Deck& d, Hand& h);
+	Deck(char* cardsInDeck[55]);
+	Deck& operator = (const Deck &d);
+	friend ostream& operator << (ostream& out, const Deck& d);
+	friend istream& operator << (istream& in, const Deck& d);
+	static void draw(Deck& d, Hand& h);
 };
