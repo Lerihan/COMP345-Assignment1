c9e64b40b7e1467157040ba9b7280e85a6d8aabe
Lerihan <phan.melina@gmail.com>
2020-10-30 03:02:20 -0400

    uncomment maploaderdriver.cpp

--- COMP345_Assignment1/MapLoaderDriver.cpp
+++ COMP345_Assignment1/MapLoaderDriver.cpp
@@ -1,17 +1,17 @@
-//#include "MapLoader.h"
-//#include <iostream>
-//#include <string>
-//
-// int main() {
-//     string dominationMap;
-// 	MapLoader* mapLoader = new MapLoader();
-//
-//    cout << "Select the map to play with: " << endl;
-//    cin >> dominationMap;
-//    cout << "Map details: " << endl;
-//
-// 	Map* map = mapLoader->GetMap(dominationMap);
-//    mapLoader->ValidateConnectedGraph(map);
-// 	std::cout << endl;
-//
-// }
+#include "MapLoader.h"
+#include <iostream>
+#include <string>
+
+ int main() {
+     string dominationMap;
+ 	MapLoader* mapLoader = new MapLoader();
+
+    cout << "Select the map to play with: " << endl;
+    cin >> dominationMap;
+    cout << "Map details: " << endl;
+
+ 	Map* map = mapLoader->GetMap(dominationMap);
+    mapLoader->ValidateConnectedGraph(map);
+ 	std::cout << endl;
+
+ }
